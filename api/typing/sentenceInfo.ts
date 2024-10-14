310fc5809c6e9f598764770182fba706f86b8266
soundraven <kin9709@gmail.com>
2024-10-15 01:01:13 +0900

    오류 확인을 위한 테스트

--- api/typing/sentenceInfo.ts
+++ api/typing/sentenceInfo.ts
@@ -5,25 +5,26 @@ import { CustomError } from "../structure/errorStructure"
 const router = express.Router()
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const [languageInfo, typeInfo] = await Promise.all([
-      pool.query(`SELECT * FROM language`),
-      pool.query(`SELECT * FROM type`),
-    ])
+  // try {
+  //   const [languageInfo, typeInfo] = await Promise.all([
+  //     pool.query(`SELECT * FROM language`),
+  //     pool.query(`SELECT * FROM type`),
+  //   ])
 
-    return res.status(200).json({
-      success: true,
-      message: "Successfully got languages.",
-      data: { languageInfo: languageInfo[0], typeInfo: typeInfo[0] },
-    })
-  } catch (error) {
-    const customError: CustomError = {
-      name: "SentenceRetrievalError",
-      message: "Failed to retrieve sentences.",
-      status: 500,
-    }
-    return next(customError)
-  }
+  //   return res.status(200).json({
+  //     success: true,
+  //     message: "Successfully got languages.",
+  //     data: { languageInfo: languageInfo[0], typeInfo: typeInfo[0] },
+  //   })
+  // } catch (error) {
+  //   const customError: CustomError = {
+  //     name: "SentenceRetrievalError",
+  //     message: "Failed to retrieve sentences.",
+  //     status: 500,
+  //   }
+  //   return next(customError)
+  // }
+  res.send("123123123")
 })
 
 export default router
