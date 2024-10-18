188cad0c6bd4531e3675121b02133e731ab2024a
soundraven <kin9709@gmail.com>
2024-10-18 15:51:50 +0900

    리프레시 토큰 DB에 저장하는 기능 추가

--- api/auth/kakaoLogin.ts
+++ api/auth/kakaoLogin.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, NextFunction } from "express"
 import { CustomError } from "../structure/errorStructure"
 import axios, { AxiosError } from "axios"
+import { pool } from "../index"
+import dayjs from "dayjs"
+import crypto from "crypto"
 
 const router = express.Router()
 
@@ -19,14 +22,15 @@ router.post(
     const { code } = req.body
 
     try {
+      console.log(process.env.KAKAO_REST_API_KEY)
       const tokenResponse = await axios.post(
         "https://kauth.kakao.com/oauth/token",
         null,
         {
           params: {
             grant_type: "authorization_code",
-            client_id: "ce7b69089aa5023470e6a5e5a89e5e48",
-            redirect_uri: "http://localhost:3000/auth/kakaologin",
+            client_id: process.env.KAKAO_REST_API_KEY,
+            redirect_uri: process.env.REDIRECT_URI,
             code,
           },
           headers: {
@@ -50,7 +54,28 @@ router.post(
           }
         )
 
-        console.log(userInfoResponse.data)
+        const encryptedRefreshToken: string = crypto
+          .createHash("sha256")
+          .update(refreshToken + process.env.REFRESH_TOKEN_SALT)
+          .digest("hex")
+        const setRefreshTokenQuery = `INSERT INTO auth (refresh_token, registered_by, expires) VALUES (?, ?, ?)`
+        const currentTime = dayjs()
+
+        const refreshTokenExpires = currentTime
+          .add(tokenResponse.data.refresh_token_expires_in, "second")
+          .format("YYYY-MM-DD HH:mm:ss")
+
+        console.log("유저정보", userInfoResponse.data)
+        console.log(userInfoResponse.data.id)
+        console.log(encryptedRefreshToken)
+
+        const [result] = await pool.query(setRefreshTokenQuery, [
+          encryptedRefreshToken,
+          userInfoResponse.data.id,
+          refreshTokenExpires,
+        ])
+
+        console.log(result)
 
         return res.status(200).json({
           message: "Successfully retrieved access token and user info.",
@@ -60,19 +85,25 @@ router.post(
           },
         })
       } else {
-        throw new Error("Failed to get access token from Kakao.")
+        return res
+          .status(400)
+          .json({ message: "Failed to get access token from Kakao." })
       }
     } catch (error) {
       const axiosError = error as AxiosError
 
+      const errorMessage =
+        (axiosError.response?.data as KakaoErrorResponse)?.error_description ||
+        "Failed to retrieve access token from Kakao."
+
+      const statusCode = axiosError.response?.status || 500
+
       const customError: CustomError = {
         name: "TokenRetrievalError",
-        message:
-          (axiosError.response?.data as KakaoErrorResponse)
-            ?.error_description ||
-          "Failed to retrieve access token from Kakao.",
-        status: axiosError.response?.status || 500,
+        message: errorMessage,
+        status: statusCode,
       }
+
       return next(customError)
     }
   }
